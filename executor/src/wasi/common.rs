84accc9d4239f2c7cba4ffc9e2a35a74678a6493
kp2pml30 <kp2pml30@gmail.com>
2025-08-25 18:23:20 +0900

    fix: remove 3 non-deterministic bytes

--- executor/src/wasi/common.rs
+++ executor/src/wasi/common.rs
@@ -74,6 +74,18 @@ pub fn read_string(memory: &GuestMemory<'_>, ptr: GuestPtr<str>) -> Result<Strin
     Ok(memory.as_cow_str(ptr)?.into_owned())
 }
 
+pub fn align_slice(slice: &mut [u8], alignment: usize) -> &mut [u8] {
+    let ptr = slice.as_ptr() as usize;
+    let aligned = (ptr + alignment - 1) & !(alignment - 1);
+    let offset = aligned - ptr;
+
+    if offset >= slice.len() {
+        return &mut [];
+    }
+
+    &mut slice[offset..]
+}
+
 pub(super) struct VFS {
     pub fds: BTreeMap<u32, FileDescriptor>,
     pub free_descriptors: Vec<u32>,
--- executor/src/wasi/preview1.rs
+++ executor/src/wasi/preview1.rs
@@ -878,8 +878,6 @@ impl generated::wasi_snapshot_preview1::WasiSnapshotPreview1 for ContextVFS<'_> 
             ),
         ];
 
-        const DIRENT_SIZE: u32 = size_of::<generated::types::Dirent>() as _;
-
         let dirent_actual_iter = direntries.iter().zip(3u64..).map(|(x, idx)| {
             let name_len: u32 = x.0.len().try_into().unwrap();
             (
@@ -901,29 +899,19 @@ impl generated::wasi_snapshot_preview1::WasiSnapshotPreview1 for ContextVFS<'_> 
         let cookie = cookie.try_into()?;
         for (ref entry, path) in head.into_iter().chain(dirent_actual_iter).skip(cookie) {
             let mut path = path.into_bytes();
-            assert_eq!(
-                1,
-                size_of_val(&entry.d_type),
-                "Dirent member d_type should be endian-invariant"
-            );
-            let entry_len = cap.min(DIRENT_SIZE);
-            let entry = entry as *const _ as _;
-            let entry = unsafe { std::slice::from_raw_parts(entry, entry_len as _) };
-            cap = cap.checked_sub(entry_len).unwrap();
-            buf = write_bytes(memory, buf, entry)?;
-            if cap == 0 {
-                return Ok(buf_len);
-            }
+            const DIRENT_SIZE_BOUND: usize = 100;
+            let mut dirent_mem_buf: [u8; DIRENT_SIZE_BOUND] =
+                [0; DIRENT_SIZE_BOUND];
 
-            if let Ok(cap) = cap.try_into() {
-                // `path` cannot be longer than `usize`, only truncate if `cap` fits in `usize`
-                path.truncate(cap);
-            }
-            cap = cap.checked_sub(path.len() as _).unwrap();
-            buf = write_bytes(memory, buf, &path)?;
-            if cap == 0 {
-                return Ok(buf_len);
-            }
+            use wiggle::GuestType;
+            let dirent_mem_buf = &mut align_slice(&mut dirent_mem_buf, generated::types::Dirent::guest_align())[..generated::types::Dirent::guest_size() as usize];
+            let mut fake_mem = wiggle::GuestMemory::Unshared(dirent_mem_buf);
+
+            fake_mem.write(wiggle::GuestPtr::<generated::types::Dirent>::new(0), entry.clone())?;
+
+            let entry_len = cap.min(dirent_mem_buf.len() as u32);
+            buf = write_bytes(memory, buf, &dirent_mem_buf[..entry_len as usize])?;
+            cap -= entry_len;
         }
         Ok(buf_len.checked_sub(cap).unwrap())
     }
