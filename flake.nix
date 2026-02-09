b1b1490081ed231a53b133171abae120696142ec
kp2pml30 <kp2pml30@gmail.com>
2026-02-09 20:27:20 +0900

    chore: split CI into two

--- flake.nix
+++ flake.nix
@@ -163,7 +163,7 @@
 								];
 								shell-hook-base = ''
 									export PATH="$(pwd)/tools/git-third-party:$PATH"
-									export LD_LIBRARY_PATH="${toString pkgs.xz.out}/lib:${toString pkgs.zlib.out}/lib:${pkgs.stdenv.cc.cc.lib}/lib:${toString pkgs.glibc}/lib:$LD_LIBRARY_PATH"
+									# export LD_LIBRARY_PATH="${toString pkgs.xz.out}/lib:${toString pkgs.zlib.out}/lib:${pkgs.stdenv.cc.cc.lib}/lib:${toString pkgs.glibc}/lib:$LD_LIBRARY_PATH"
 									export LLVM_PROFILE_FILE=/dev/null
 								'';
 							in
